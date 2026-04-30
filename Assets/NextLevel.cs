fac458eb04ae770f6cb7a4cb69c6c9876f876635
ShaddosAmigos <shaddimedia@gmail.com>
2026-04-30 13:25:24 +0200

    flaga

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
